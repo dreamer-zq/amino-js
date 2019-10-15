61b5ba1d121225f63f23926b40bdb7fdc2cf4794
zhangzhiqiang <745124335@qq.com>
2019-10-15 18:10:40 +0800

    refactor test code

--- test/codec.test.js
+++ test/codec.test.js
@@ -60,52 +60,73 @@ describe('codec', () => {
     assert.equal(JSON.stringify(stdTx), JSON.stringify(stdTx2))
   })
 
-  it('decode MsgDelegate', () => {
+  it('encode and decode MsgDelegate', () => {
     const tx = '+gHZHnawClZq9q87ChRvnmOH06Nbe/HjZ3UHiJvFNIoqWBIUQUjWqc1EICBEmntpvID7SMRea2UaJAoJaXJpcy1hdHRvEhc1NjQ4ODAwMDAwMDAwMDAwMDAwMDAwMBIlCh8KCWlyaXMtYXR0bxISNjAwMDAwMDAwMDAwMDAwMDAwEKCNBhpvCibrWumHIQNzWoVZHQoK31cW7odpxs23ONYfYwG4LcoM5qeR/Uxm9hJAV1ZYiGClvu3eI/HjkYyEYCNqpSUoG7EbUf+gIfKMGrhebhhsrZr0CGAJC7FkOY89a2A7yDmloqEbeUZkAnmdtBjERyAIIgQ5ODg3'
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 
-  it('decode MsgBeginUnbonding', () => {
+  it('encode and decode MsgBeginUnbonding', () => {
     const tx = '8gHZHnawClRT7PhYChTdgrgCKoPmoT9SdJLS8twmPJMRGRIUsPscdczRrb6Pmuq0nJuuqU7DynQaIjEwOTQ5OTg0OTkxMTM1MDgyMDY3Nzk1MTAwMDAwMDAwMDASJQofCglpcmlzLWF0dG8SEjQwMDAwMDAwMDAwMDAwMDAwMBDQhgMabwom61rphyEDk4uM/r0JDdyhjsz/ex9orRFnJTuh8tH9pOgFZNDkyFgSQABDQ76Vkdo6e9EGd3xNBjBDtiVY0dVZ3+vWPrsT14sBGD3EX9OE5KHv+rKHjgR50sjZdUYjYyq9v94Lp2VFmJ0YskAgSg=='
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 
-  it('decode MsgBeginRedelegate', () => {
+  it('encode and decode MsgBeginRedelegate', () => {
     const tx = 'hALZHnawCmbmQ/puChQC9+u6uqusOvo2ODkX3Qy6lI6tthIUQUjWqc1EICBEmntpvID7SMRea2UaFNB+E+NptKEAqUpfQaCzW7bPigelIh4xMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDASJQofCglpcmlzLWF0dG8SEjYwMDAwMDAwMDAwMDAwMDAwMBCgjQYabwom61rphyEDsDWWEH8Vk26m7kAeWEvFs2KCA2N3wkcS5buhvO00QiMSQKpYIN8aR2DXhLVWjfkl5ogdWXjtJh+TCPBb3pm6Tmz6RWWvd/E2mOOmEBuNwj72u3vs6t+goszgtcJxrLyLwWgY8wEgIA=='
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 
-  it('decode MsgWithdrawDelegatorRewardsAll', () => {
+  it('encode and decode MsgWithdrawDelegatorRewardsAll', () => {
     const tx = 'uAHZHnawChqx48v2ChTRWOI+bQmvN3llDpuBRcgFX52qIxIlCh8KCWlyaXMtYXR0bxISMzAwMDAwMDAwMDAwMDAwMDAwENCGAxpvCibrWumHIQJh6kuzo3CwUw56hUps57pBj6ibCASnEVQnmYdf5OUnUBJAilxVDNFtccovePZxN0lO3et7gYTziynRkBlTU+G3yMBXbU/B2lZKLJcyx1iNAKHIDgKe/yIJgNacP7Jil6wrqRjWRSAU'
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 
-  it('decode MsgWithdrawDelegatorReward', () => {
+  it('encode and decode MsgWithdrawDelegatorReward', () => {
     const tx = '5QHZHnawCjAgaOF3ChR+paZ+hdKVgHIHvajf4puIH3jinxIUQUjWqc1EICBEmntpvID7SMRea2USJQofCglpcmlzLWF0dG8SEjMwMDAwMDAwMDAwMDAwMDAwMBDQhgMabwom61rphyEDeH+Mr6ANGac8ePCw12l1AMyHCmLXrWbq5d45+XEI5nMSQMtFKGYmPYM5ZOF3ZfmMd7vjcm1IySBK12y+gtXYHmXiJYHFftQ5oaCIkGzAxS7Yw296FXgazcGNhkeCoPdpFpkYgEQgIiIV5aeU5omY5Lq66aKG5Y+W5aWW5Yqx'
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 
-  it('decode MsgDeposit', () => {
+  it('encode and decode MsgDeposit', () => {
     const tx = '3wHZHnawCkFGLTT6CAQSFKnd9CBCEFz4g9dvcZQWtt+ZdbqNGiMKCWlyaXMtYXR0bxIWMTAwMDAwMDAwMDAwMDAwMDAwMDAwMBIlCh8KCWlyaXMtYXR0bxISNDAwMDAwMDAwMDAwMDAwMDAwENCGAxpvCibrWumHIQJwkFdozku6b74ivnWky3baFFC0L49dnp5hSgvPOGNMqhJAKtZNyGb9Ee+b5zNUGh9DbrzRhPZipUQ+JbydOk5QMww3Yrg7MYIwNVJ9PVwxeyruBRUb/cQGo9LVxID4ecVX0RjZQCAr'
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 
-  it('decode MsgVote', () => {
+  it('encode and decode MsgVote', () => {
     const tx = 'vAHZHnawCh4KuBjmCAQSFIUDainSsTukx5MtFvxLIXZ+4wE3GAMSJQofCglpcmlzLWF0dG8SEjQwMDAwMDAwMDAwMDAwMDAwMBDQhgMabwom61rphyECssg4jFHxaEV7uMZTNtyIipBVSGs5w7GErB9sxehYYGwSQDSqwCD50S3DJZTz9+kX49VvqGz7AaJqTPFEVV3uQPchf0Hj7PW2/0oAgQk5admpCcu14AHzv7THT4igOZtpSIsYsAIgIA=='
     const txBz = new Buffer(tx, 'base64')
     const stdTx = Codec.unMarshalBinaryLengthPrefixed(txBz)
     console.log(JSON.stringify(stdTx))
+
+    const bz = Codec.marshalBinaryLengthPrefixed(stdTx)
+    assert.equal(Buffer.from(bz).toString("base64"), tx)
   })
 })
